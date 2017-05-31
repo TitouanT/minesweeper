e4fb2fb524b05107f24c75657899f2b1fbd7848a
TitouanT <titouan.teyssier@gmail.com>
2017-05-31 12:18:58 +0200

    change smth

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@ let game;
 let counter;
 
 function setup() {
-	let canvas = createCanvas (602, 602); // width, height
+	let canvas = createCanvas(602, 602); // width, height
 	canvas.parent('p5sketch');
 	createCounter();
-	createNewGame(10,10,15);
+	createNewGame(10, 10, 15);
 	myCreateButton('Beginner', 9, 9, 10);
 	myCreateButton('Medium', 16, 16, 40);
 	myCreateButton('Expert', 16, 30, 99);
@@ -16,37 +16,34 @@ function draw() {
 	game.show();
 }
 
-function windowResized () {
+function windowResized() {
 	myResizeCanvas();
 }
 
-function mouseClicked () {// use millis to time event
+function mouseClicked() { // use millis to time event
 
-	let line = game.mouseY2line (mouseY);
-	let col = game.mouseX2col (mouseX);
+	let line = game.mouseY2line(mouseY);
+	let col = game.mouseX2col(mouseX);
 	let cell = game.get(line, col);
 	if (cell != null) {
 
 		if (mouseButton === LEFT) {
 			if (game.gameOver || game.win) {
 				createNewGame();
-			}
-			else {
+			} else {
 
 				if (!game.minesPlaced) {
 					game.placeMine(line, col);
 				}
 				game.reveal(line, col);
 			}
-		}
-		else if (mouseButton === RIGHT) {
+		} else if (mouseButton === RIGHT) {
 			if (!cell.showed) {
 				cell.marked = !cell.marked;
 				if (cell.marked) game.marked++;
 				else game.marked--;
 			}
-		}
-		else if (mouseButton === CENTER) {
+		} else if (mouseButton === CENTER) {
 			game.revealNeighbor(line, col);
 		}
 
@@ -60,6 +57,6 @@ function mouseClicked () {// use millis to time event
 function myResizeCanvas() {
 	let lines = game.grid.getLines();
 	let cols = game.grid.getCols();
-	let cell_size = min ((0.8*windowWidth)/cols, windowHeight/lines);
-	resizeCanvas (cell_size * cols, cell_size * lines);
+	let cell_size = min((0.8 * windowWidth) / cols, windowHeight / lines);
+	resizeCanvas(cell_size * cols, cell_size * lines);
 }
